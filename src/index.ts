02a4bedacf7c5acef47e3e47d259109a82046d9c
Sebastian <sebastiangseijo@gmail.com>
2020-08-24 20:35:57 +0200

    Add main file. add mikro orm init configuration.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('init');
+import { MikroORM } from "@mikro-orm/core";
+import {__db_name__, __db_type__, __prod__} from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: __db_name__,
+    debug: __prod__,
+    type: __db_type__,
+  });
+};
+
+main();
