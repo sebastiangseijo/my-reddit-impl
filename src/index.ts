878c16da40512a90379b467fed09175aedf35176
Sebastian <sebastiangseijo@gmail.com>
2020-08-24 21:36:14 +0200

    Add Apollo Server Initial Structure.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import { MikroORM } from "@mikro-orm/core";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/Post";
 
 const main = async () => {
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   // const post = orm.em.create(Post, { title: "my first post" });
   // await orm.em.persistAndFlush(post);
   // const posts = await orm.em.find(Post, {});
   // console.log(posts);
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
