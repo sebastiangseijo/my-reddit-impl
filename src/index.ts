5fe9a7cb6b5c056b6fea40710621376811bfe909
Sebastian <sebastiangseijo@gmail.com>
2020-08-24 21:20:43 +0200

    Add server main dependencies: graphql, express, apollo-server

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
-  await orm.em.nativeInsert(Post, { title: " my first post x2" });
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: "my first post" });
+  // await orm.em.persistAndFlush(post);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch((err) => {
