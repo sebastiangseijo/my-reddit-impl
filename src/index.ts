bbab5bd263c8b9e459a7856306a1bc7e906db5ed
Sebastian <sebastiangseijo@gmail.com>
2020-08-27 01:12:24 +0200

    Make main class instead of function.

--- src/index.ts
+++ src/index.ts
@@ -7,35 +7,39 @@ import { PostResolver } from "./resolvers/Post";
 import { ApolloServer } from "apollo-server-express";
 import { UserResolver } from "./resolvers/user";
 
-const main = async () => {
-  const app = express();
-  const orm = await MikroORM.init(microConfig);
-  await orm.getMigrator().up();
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [PostResolver, UserResolver],
-      validate: false,
-    }),
-    context: () => ({
-      em: orm.em,
-    }),
-  });
+export class Main {
+  async initServer() {
+    const app = express();
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+    const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [PostResolver, UserResolver],
+        validate: false,
+      }),
+      context: () => ({
+        em: orm.em,
+      }),
+    });
 
-  apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({ app });
 
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+    // const post = orm.em.create(Post, { title: "my first post" });
+    // await orm.em.persistAndFlush(post);
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
 
-  app.get("/", (_, res) => {
-    res.send("hello");
-  });
-  app.listen(4000, () => {
-    console.log("server started on localhost:4000");
-  });
-};
+    app.get("/", (_, res) => {
+      res.send("hello");
+    });
+    app.listen(4000, () => {
+      console.log("server started on localhost:4000");
+    });
+  }
+}
 
-main().catch((err) => {
+const main = new Main();
+
+main.initServer().catch((err) => {
   console.error(err);
 });
