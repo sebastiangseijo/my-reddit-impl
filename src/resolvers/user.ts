8245b1da73a8c7c790eaf3f1b2b8d6b46f9d456a
Sebastian <sebastiangseijo@gmail.com>
2020-08-27 01:02:54 +0200

    Add user entity and resolver.

---
+++ src/resolvers/user.ts
@@ -0,0 +1,30 @@
+import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { MyContext } from "../types";
+import { User } from "../entities/User";
+import argon2 from "argon2";
+
+@InputType()
+export class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => User)
+  async register(
+    @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<User> {
+    // hash password before updating database with argon2 algorithm
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
+}
