4ef315ef9e059b53cfc5cf1655c1fe4c98bccf3d
Sebastian <sebastiangseijo@gmail.com>
2020-08-27 01:04:30 +0200

    Add entity User.

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { Post } from "./entities/Post";
 import { __db_name__, __db_type__, __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
-  entities: [Post],
   migrations: {
     path: path.join(__dirname, "./migrations"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
   },
+  entities: [Post, User],
   dbName: __db_name__,
   debug: !__prod__,
   type: __db_type__,
